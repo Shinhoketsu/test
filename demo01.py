5a13dc717359d4121b16c1076b974ba1d0d4a8c3
shinhoketsu <462865515@qq.com>
2022-05-27 17:14:13 +0800

    第二天

--- demo01.py
+++ demo01.py
@@ -1,23 +1,52 @@
-"""
-print("hello world!")
-print(2)
-print(3333) #11111
+# """
+# print("hello world!")
+# print(2)
+# print(3333) #11111
 
-a=input("请输入内容")
-print("输入内容:",a)
-"""
-from argparse import ONE_OR_MORE
+# a=input("请输入内容")
+# print("输入内容:",a)
+# """
+# from argparse import ONE_OR_MORE
 
-# print(type(1))
-# print(type([]))
-# print(type(ONE_OR_MORE))
-# a=str("56")
-# print(type(a))
+# # print(type(1))
+# # print(type([]))
+# # print(type(ONE_OR_MORE))
+# # a=str("56")
+# # print(type(a))
 
-# a=float(input("请输入："))
-# b=float(input("请输入2："))
-# print("获得内容:",a+b)
+# # a=float(input("请输入："))
+# # b=float(input("请输入2："))
+# # print("获得内容:",a+b)
 
-a=input("请输入:")
-b=input("请输入2：")
-print(len(a+b))
+# a=input("请输入:")
+# b=input("请输入2：")
+# print(len(a+b))
+
+# a=(555,777,999,"啊",False,9,8,5,1)
+# print(a[0:5])
+# print(a[5:7])
+# print(a[7:])
+
+
+# a = [1,2,3,4,5,6,7,8,9]
+# a.append(10)
+# a.insert(5,10)
+# b=a.pop(0)
+# print(a)
+
+
+# a = {"name":"pob","age":"15","sex":"none"}
+# print(a.get("name"))
+# b=(a.pop("name"))
+# print (b)
+# a.update(name=1)
+# print(a)
+# del a["name"]
+# print(a)
+
+
+a=input("请输入名字")
+b=input("请输入年龄")
+c=input("请输入性别")
+d={"name":a,"age":b,"sex":c}
+print(d)
