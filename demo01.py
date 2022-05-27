53638a03d260daf244e3743484f16053b8f3f280
shinhoketsu <462865515@qq.com>
2022-05-27 15:21:12 +0800

    第一次向github提交代码

---
+++ demo01.py
@@ -0,0 +1,24 @@
+"""
+print("hello world!")
+print(2)
+print(3333) #11111
+
+a=input("请输入内容")
+print("输入内容:",a)
+"""
+from argparse import ONE_OR_MORE
+
+
+# print(type(1))
+# print(type([]))
+# print(type(ONE_OR_MORE))
+# a=str("56")
+# print(type(a))
+
+# a=float(input("请输入："))
+# b=float(input("请输入2："))
+# print("获得内容:",a+b)
+
+a=input("请输入:")
+b=input("请输入2：")
+print(len(a+b))
